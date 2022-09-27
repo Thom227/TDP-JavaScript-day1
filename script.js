41e7fd09d809f4222eb01315b748f9cd59c0f06c
Thom227 <sms202112@outlook.com>
2022-09-27 13:39:12 +0100

    save file1

--- script.js
+++ script.js
@@ -62,17 +62,90 @@ console.log(str2);
 let str3 = `5 + 3 = ${5 + 3}`;
 console.log(str3)
 
-let a;
-let b = "12345";
-let c = 12344;
-let d = true;
-let e = {a:"JavaScript"};
+let aa;
+let bb = "12345";
+let cc = 12344;
+let dd = true;
+let ee = {aa:"JavaScript"};
 
-console.log(e)
+console.log(ee)
 
 let totalMoney = 4000;
 let moneyPaidSoFar = 2348;
 let totalLeftToPay = totalMoney - moneyPaidSoFar
 
-console.log(`The total bill is £${totalMoney} the 
-remaining amount of money to be paid is £${totalLeftToPay}`);
+console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
+
+let a = 1;
+let b = 1;
+
+a
+++
+b
+console.log(`a = ${a}\nb = ${b}`);
+
+let c = 1;
+let d = 1;
+
+c++; d;
+
+console.log(`c = ${c}\nd = ${d}`);
+
+//Iteration == loops
+//FOR loop runs while a condition is met
+//i for iteration = convetional way just
+
+for(let i = 0; i < 3; i++) {
+    console.log(`i = ${i}`);
+}
+
+//WHILE loop work with BOOLEANs, run untill a 
+//condition is no longer true
+let  condition = true;
+let increment = 0;
+
+while(condition === true) {
+    console.log(`increment = ${increment}`);
+    increment ++;
+
+    if(increment === 3) {
+        condition = false;
+    }
+}
+
+//DO-WHILE is like a while loop but will ALWAYS
+//run atleast once
+
+let z = false;
+do{
+    console.log("Run forest run");
+} while(z === true);
+
+//SWITCH CASES will run a break or return
+//Can be much more effective than large if-else
+//if statements
+
+let num =3;
+switch(num) {
+    case 0:
+        console.log("Number is zero");
+        break;
+    case 1:
+        console.log("We are number one");
+        break;
+    case 3:
+        console.log("We're at 3");
+        break;
+    case 4:
+        console.log("We made it to 4");
+        break;
+    default:
+        console.warn("Not here");
+}
+
+let A = 100;
+
+while(A <=200) {
+    console.log(A);
+    A ++;
+}
